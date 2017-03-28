473c7a883c552165bedebd2598cf72d1e8e5739a
hzhxm <526833465@qq.com>
2017-03-28 23:37:53 +0800

    v1.5 add login and register page

---
+++ smysc/action/login.php
@@ -0,0 +1,133 @@
+<?php
+
+//通用配置
+require_once "../dbclass.php";
+
+/*
+$errstr = "no error";
+$flag = false;
+//连接数据库
+$db = new dbclass("jim");
+$connect = $db->sql_init();
+
+$username = '333';
+$password = 'zyx';
+
+ $sql = "select * from `user_information`
+            where (`userAccountName` ='" . $username . "' or `userMobile` = '" . $username . "') and `userPassword` = '" . $password . "'";
+
+echo $sql;
+    $result = $db->my_query($connect, $sql);
+
+    if ($result) {
+        while ($row = $db->my_fetch_array($result, MYSQLI_BOTH)) {
+            $userid = $row["userID"];
+            $flag = true;
+        }
+
+    } else {
+        $errstr = "error to query : " . mysqli_error($connect);
+    }
+
+//echo $productlist ;
+//关闭数据库
+$db->close_connect($connect);
+
+//$totalPrice = ((int)$productPrice) ;//* $num;
+//配置ajax返回数据
+$data = array(
+    "flag" => $flag,
+    "error" => $errstr,
+);
+echo json_encode($data);
+die;
+*/
+
+$action = $_POST['action'];
+
+if($action == "loginAuth")
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    loginAuth($username,$password);
+}
+elseif($action == "registerUser")
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $phone = $_POST['phone'];
+
+    registerUser($username,$password,$phone);
+}
+
+function loginAuth($username,$password){
+    $errstr = "no error";
+    $flag = false;
+    //连接数据库
+    $db = new dbclass("jim");
+    $connect = $db->sql_init();
+
+    $sql = "select * from `user_information`
+            where (`userAccountName` ='" . $username . "' or `userMobile` = '" . $username . "') and `userPassword` = '" . $password . "'";
+
+    $result = $db->my_query($connect, $sql);
+
+    if ($result) {
+        while ($row = $db->my_fetch_array($result, MYSQLI_BOTH)) {
+            $userid = $row["userID"];
+            $flag = true;
+        }
+
+    } else {
+        $errstr = "error to query : " . mysqli_error($connect);
+    }
+
+    //echo $productlist ;
+    //关闭数据库
+    $db->close_connect($connect);
+
+    //$totalPrice = ((int)$productPrice) ;//* $num;
+    //配置ajax返回数据
+    $data = array(
+        "flag" => $flag,
+        "error" => $errstr,
+    );
+    echo json_encode($data);
+}
+
+function registerUser($username,$password,$phone){
+    $errstr = "no error";
+    $flag = false;
+    //连接数据库
+    $db = new dbclass("jim");
+    $connect = $db->sql_init();
+
+
+    $sql = "INSERT INTO `smysc`.`user_information` (`userLevel`,`userWechatNickName`,`userAccountName`,`userPassword`,
+            `userMobile`,`UserPassportID`,`UserDescription`,`label`)
+            VALUES(0,null,'". $username . "','" . $password . "','". $phone . "',null,null,null)";
+
+    $result = $db->mysql_insert($connect, $sql);
+
+    if ($result) {
+        $flag = true;
+    } else {
+        $errstr = "error to insert : " . mysqli_error($connect);
+    }
+
+    //echo $productlist ;
+    //关闭数据库
+    $db->close_connect($connect);
+
+    //$totalPrice = ((int)$productPrice) ;//* $num;
+    //配置ajax返回数据
+    $data = array(
+        "flag" => $flag,
+        "error" => $errstr,
+    );
+    echo json_encode($data);
+}
+
+
+?>
---
+++ smysc/js/login.js
@@ -0,0 +1,84 @@
+/**
+ * Created by zhengjinguo on 2017/3/27.
+ */
+$(document).ready(function(){
+    $("button#login-btn").bind("click",function(){
+    //$("button#login-btn").click(function(){
+        var username = $(".delu-li1 input").val();
+        var password = $(".delu-li2 input").val();
+        if(username == "" || password == "")
+        {
+            alert("请输入账号／密码! ");
+            return;
+        }
+
+        $.ajax({
+            type: 'post',
+            async: true,
+            url: 'action/login.php',
+//       contentType: false,    //不可缺
+//      processData: false,    //不可缺
+            data: {action: "loginAuth", username: username,password: password},
+            dataType: "json",
+            success: function (data, status) {
+                //ajax 成功
+                alert(data.flag);
+                if(data)
+                {
+
+                }
+                else
+                {
+                    alert("账号或密码错误！");
+                }
+            },
+            error: function (data, status, e) {
+                console.log("系统异常" + data + e);
+            },
+            beforeSend: function (xhr) {
+            }
+        });
+    });
+
+
+    $("button.sczc-btn1").bind("click",function(){
+        var regname = $("#register-name").val();
+        var regpassword = $("#register-password").val();
+        var regphone = $("#register-phone").val();
+        if(regname == "" || regpassword == "" || regphone == "" )
+        {
+            alert("请输入账号／密码／手机号! ");
+            return;
+        }
+
+        $.ajax({
+            type: 'post',
+            async: true,
+            url: 'action/login.php',
+//       contentType: false,    //不可缺
+//      processData: false,    //不可缺
+            data: {action: "registerUser", username: regname,password: regpassword,phone:regphone},
+            dataType: "json",
+            success: function (data, status) {
+                //ajax 成功
+                if(data.flag)
+                {
+                    location.href="login.html";
+                }
+                else
+                {
+                    alert("注册失败！");
+                }
+            },
+            error: function (data, status, e) {
+                console.log("系统异常" + data + e);
+            },
+            beforeSend: function (xhr) {
+            }
+        });
+    });
+
+    $("#to-register").click(function(){
+        location.href="register.html";
+    });
+});
